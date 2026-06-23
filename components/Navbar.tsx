1bd22aa2cb8067b458e1373b8d61dd1e2580a189
Abinaya Palanisamy <142603350+Abinaya-2023@users.noreply.github.com>
2026-06-23 11:29:06 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link href="/" className="flex items-center">
             <Image
-              src="public/images/sandhata.jpg"
+              src="public/images/Sandhata.jpg"
               alt="Sandhata Logo"
               width={250}
               height={80}
